5583fd2d14dbb58b16806b9b9131b4239b036ee2
Zero-up1218 <1711399365@qq.com>
2026-06-16 17:30:55 +0800

    refactor: Update search page to use async API search methods

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,26 +1,29 @@
-//index.js
-//获取应用实例
+// pages/search/index.js
+// 搜索页面 - 集成 API 搜索功能
+
 const app = getApp();
+
 Page({
   data: {
     spriteList: [],
+    isLoading: false,
+    searchQuery: '',
+    hasError: false,
+    errorMsg: ''
   },
+
   onLoad: function (options) {
-    this.isLoading = true;
-    wx.showLoading({
-      mask: true,
-      title: '数据加载中'
-    });
     this.filterList = app.globalObject.spriteList;
   },
+
   onReady: function () {
-    wx.hideLoading();
-    this.isLoading = false;
     this.windowHeight = wx.getSystemInfoSync().windowHeight;
     this.scrollList = [];
   },
+
   onScroll: function (e) {
   },
+
   tapSprite: function (event) {
     var index = event.currentTarget.dataset.index + '';
     index = '';
@@ -35,54 +38,96 @@ Page({
       });
     }
   },
+
   tapFilter: function (event) {
     wx.navigateTo({
       url: '/pages/filter/index?isFilter=' + this.data.isFilter + '&filter=' + this.data.filter
     });
   },
+
   toabout: function () {
     wx.navigateTo({
       url: '/pages/about/index'
     });
   },
-  back:function() {
-    wx.navigateBack({ 
-    });
+
+  back: function () {
+    wx.navigateBack();
   },
+
   clearSearch: function () {
     this.setData({
-      spriteList: this.filterList
+      spriteList: [],
+      searchQuery: '',
+      hasError: false,
+      errorMsg: ''
     });
   },
+
   onInput: function (event) {
-    var vaule = event.detail.value.replace(/\s/ig, '');
-    if (this.searchStr == vaule) {
-      return vaule;
+    var value = event.detail.value.replace(/\s/ig, '');
+    
+    if (this.searchStr == value) {
+      return value;
     }
-    if (/^\d+$/.test(vaule) && !/^\d{3}$/.test(vaule)) {
-      return vaule;
+
+    // 验证输入
+    if (/^\d+$/.test(value) && !/^\d{1,5}$/.test(value)) {
+      return value;
     }
-    if (vaule) {
-      var spriteList = this.search('cname,id', vaule);
-      console.log(vaule, spriteList);
+
+    this.searchStr = value;
+
+    if (!value) {
       this.setData({
-        spriteList: spriteList
+        spriteList: [],
+        searchQuery: '',
+        hasError: false,
+        errorMsg: ''
       });
-    } else {
-      this.setData({
-        spriteList: []
-      });
+      return;
     }
-    return this.searchStr = vaule;
+
+    // 显示加载状态
+    this.setData({
+      isLoading: true,
+      searchQuery: value
+    });
+
+    // 优先使用 API 搜索
+    app.globalObject.searchAsync('cname,id,name', value)
+      .then((results) => {
+        this.setData({
+          spriteList: results || [],
+          isLoading: false,
+          hasError: false,
+          errorMsg: ''
+        });
+        console.log('Search results:', results);
+      })
+      .catch((err) => {
+        console.error('Search failed:', err);
+        // 降级到本地搜索
+        const localResults = this.search('cname,id', value);
+        this.setData({
+          spriteList: localResults,
+          isLoading: false,
+          hasError: false,
+          errorMsg: ''
+        });
+      });
   },
-  // 多属性查询精灵
+
+  /**
+   * 本地搜索方法（备用）
+   */
   search: function (key, queryString) {
     var results = [];
     var keys = key.split(',');
     for (var i = 0, size = this.filterList.length; i < size; i++) {
       var item = this.filterList[i];
       for (var j = 0, jsize = keys.length; j < jsize; j++) {
-        if (item[keys[j]].toLowerCase().indexOf(queryString.toLowerCase()) >= 0) {
+        if (item[keys[j]] && item[keys[j]].toString().toLowerCase().indexOf(queryString.toLowerCase()) >= 0) {
           results.push(item);
           break;
         }
